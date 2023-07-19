82598284241f3afff93b3a66092f9cad067e3768
annigodhani <aniruddhgodhani@gmail.com>
2023-07-19 11:48:16 +0530

    19/07

---
+++ flight.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+class Fuel
+{
+    private:
+    int flightNo;
+    char destination;
+    float distance,Fuel;
+
+    int CALFUEL()
+    {
+        if (distance<=1000)
+        {
+            Fuel=500;
+        }
+        else if (distance>1000 && distance<=2000)
+        {
+            Fuel=1100;
+        }
+        else
+        {
+           Fuel=2200;
+        }
+    }
+    public:
+    void FEEDINFO() 
+    {
+        cout<<"Enter flight no:";
+        cin>>flightNo;
+        cout<<"Enter Destination:";
+        cin>>destination;
+        cout<<"Enter Distance:";
+        cin>>distance;
+
+        CALFUEL();
+    }
+    void SHOWINFO()
+    {
+        cout<<"Flight number:"<<flightNo<<endl;
+        cout<<"Destinatio:"<<destination<<endl;
+        cout<<"Distance:"<<distance<<"km"<<endl;
+        cout<<"Fuel:"<<Fuel<<"litter"<<endl;
+    }
+};
+int main()
+{
+    Fuel f;
+    f.FEEDINFO();
+    f.SHOWINFO();
+}
