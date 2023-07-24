4cabb35a60e6fd06ccb43faf7b821e9b1c97c152
annigodhani <aniruddhgodhani@gmail.com>
2023-07-24 18:08:32 +0530

    24/07

---
+++ una3.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+class Number {
+ int real,img;
+ public:
+ void Setdata(int a,int k)
+ {
+    real=a,img=k;
+ }
+ void Getdata()
+ {
+    cout<<"Num is:"<<real<<"-"<<img<<"i"<<endl;
+ }
+ Number operator--()
+ {
+ Number t;
+ t.real=--real;
+ t.img=--img;
+ return t;
+ }
+};
+ int main()
+ {
+    Number n,m;
+    n.Setdata(2,5);
+    n.Getdata();
+    --n;
+    m=n;
+    m.Getdata();
+}
